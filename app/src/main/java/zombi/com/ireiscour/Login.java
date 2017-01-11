3f43810cd881fd60750ab509a2ced4390d7e675b
zombi42 <zombi42@hotmail.fr>
2017-01-11 09:32:44 +0100

    Sauvegarde Info

--- app/src/main/java/zombi/com/ireiscour/Login.java
+++ app/src/main/java/zombi/com/ireiscour/Login.java
@@ -2,6 +2,7 @@ package zombi.com.ireiscour;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,15 +17,31 @@ public class Login extends AppCompatActivity {
         context=this;
         setContentView(R.layout.activity_login);
         Button v_b_ok= (Button) findViewById(R.id.OK_ID);
+        SharedParaChoix.RecupChoixStatic(context);
+        EditText v_t_id= (EditText) findViewById(R.id.identifiant);
+        EditText v_t_pwd=(EditText) findViewById(R.id.motdepasse);
+        if(ChoixStatic.getInstance().Identifiant!=null)
+        {
+            v_t_id.setText(ChoixStatic.getInstance().Identifiant);
+
+        }
+        if(ChoixStatic.getInstance().MotdePasse!=null)
+        {
+            v_t_pwd.setText(ChoixStatic.getInstance().MotdePasse);
+
+        }
+
         v_b_ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText v_t_id= (EditText) findViewById(R.id.identifiant);
                 EditText v_t_pwd=(EditText) findViewById(R.id.motdepasse);
+
                 ChoixStatic CS=ChoixStatic.getInstance();
                 CS.Identifiant=v_t_id.getText().toString();
                 CS.MotdePasse=v_t_pwd.getText().toString();
                 Intent v_i = new Intent(context, Choix.class);
+                SharedParaChoix.SaveChoixStatic(context);
                 v_i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 getApplicationContext().startActivity(v_i);
             }
---
+++ app/src/main/java/zombi/com/ireiscour/SharedParaChoix.java
@@ -0,0 +1,40 @@
+package zombi.com.ireiscour;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by maxime on 09/12/16.
+ */
+
+public class SharedParaChoix {
+    private SharedParaChoix()
+    {
+
+    }
+
+    public static void RecupChoixStatic(Context context)
+    {
+
+            SharedPreferences setting=context.getSharedPreferences("IREIS",0);
+            ChoixStatic.getInstance().Identifiant=setting.getString("ID",null);
+        ChoixStatic.getInstance().MotdePasse=setting.getString("PWD",null);
+        ChoixStatic.getInstance().Formation=setting.getString("FORM",null);
+        ChoixStatic.getInstance().Lieu=setting.getInt("WHERE",99);
+        ChoixStatic.getInstance().Promotion=setting.getString("PROMO",null);
+        }
+    public static void SaveChoixStatic(Context context)
+    {
+        SharedPreferences setting=context.getSharedPreferences("IREIS",0);
+        SharedPreferences.Editor editor = setting.edit();
+        editor.putString("ID",ChoixStatic.getInstance().Identifiant);
+        editor.putString("PWD",ChoixStatic.getInstance().MotdePasse);
+        editor.putString("FORM",ChoixStatic.getInstance().Formation);
+        editor.putInt("WHERE",ChoixStatic.getInstance().Lieu);
+        editor.putString("PROMO",ChoixStatic.getInstance().Promotion);
+        editor.commit();
+
+    }
+
+
+}
